e9b5400c7938cced86e5b75f038f361ad240b895
nasso <nassomails@gmail.com>
2021-10-09 02:22:07 +0200

    Modify "counter" example to display counted values

--- examples/counter.scm
+++ examples/counter.scm
@@ -1,15 +1,19 @@
+(define print (lambda xs
+  (if (null? xs)
+    (newline)
+    ((lambda ()
+      (display (car xs))
+      (apply print (cdr xs)))))))
+
 (define make-counter
-    (lambda (value)
-        (lambda ()
-            (set! value (+ value 1))
-            value
-        )
-    )
-)
+  (lambda (value)
+    (lambda ()
+      (set! value (+ value 1))
+      value)))
 
 (define my-counter (make-counter 10))
 (define my-other-counter (make-counter 20))
-(my-counter)
-(my-other-counter)
-(my-counter)
-(my-other-counter)
+(print "my-counter: " (my-counter))
+(print "my-other-counter: " (my-other-counter))
+(print "my-counter: " (my-counter))
+(print "my-other-counter: " (my-other-counter))
