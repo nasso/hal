8152d5b76e61e69506be6c2db8752e90d15047f6
nasso <nassomails@gmail.com>
2021-10-16 03:51:14 +0200

    Update continuations and counter examples

--- examples/counter.scm
+++ examples/counter.scm
@@ -1,15 +1,19 @@
-(define print (lambda xs
+(define (print . xs)
   (if (null? xs)
     (newline)
     (begin
       (display (car xs))
-      (apply print (cdr xs))))))
+      (apply print (cdr xs))
+    )
+  )
+)
 
-(define make-counter
-  (lambda (value)
-    (lambda ()
-      (set! value (+ value 1))
-      value)))
+(define (make-counter value)
+  (lambda ()
+    (set! value (+ value 1))
+    value
+  )
+)
 
 (define my-counter (make-counter 10))
 (define my-other-counter (make-counter 20))
