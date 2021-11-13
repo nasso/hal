358cef506f55a79bf9cbc1a1d18246bd7a2aefb3
nasso <nassomails@gmail.com>
2021-11-13 01:14:03 +0100

    Update tests to use better syntax

--- examples/core-tests.scm
+++ examples/core-tests.scm
@@ -11,203 +11,126 @@
   (display
     "This shouldn't be displayed. Do you support datum comments (#;)?")
 
-; Wraps a function to be run as a test case as part of a test suite.
-(define test (lambda (name fn)
-  (lambda ()
-    ((lambda (success)
-      (if success
-        (display "  [OK] ")
-        (display "  [FAIL] ")
-      )
-      (display name)
-      (newline)
-      success
-    ) (not (not (fn))))
-  )
-))
-
-; Defines a test suite.
-(define suite (lambda (name . tests)
-  (lambda (on-pass on-fail)
-    (display "[")
-    (display name)
-    (display "]")
-    (newline)
-
-    (
-      (lambda (f) (f f tests))
-      (lambda (rec tests)
-        (if (null? tests)
-          (void)
-          (begin
-            (if ((car tests))
-              (on-pass)
-              (on-fail)
-            )
-            (rec rec (cdr tests))
-          )
-        )
-      )
-    )
-  )
-))
-
 ; Run a series of test suites and display a summary.
-(define test-suites (lambda suites
-  ((lambda (passes fails)
-    (
-      (lambda (f) (f f suites))
-      (lambda (rec suites)
-        (if (null? suites)
-          (void)
-          (begin
-            ((car suites)
-              (lambda () (set! passes (+ passes 1)))
-              (lambda () (set! fails (+ fails 1)))
-            )
-            (rec rec (cdr suites))
-          )
-        )
-      )
-    )
+(define-syntax test-suites (syntax-rules ()
+  [(_ (suite_name (test_name test_body) ...) ...)
+    (let ([__passes 0] [__fails 0])
+      ; run all suites
+      (begin
+        (display "[")
+        (display suite_name)
+        (display "]")
+        (newline)
 
-    (newline)
-    (display "SUMMARY: ")
-    (display passes)
-    (display " tests passed, ")
-    (display fails)
-    (display " failed")
-    (newline)
-  ) 0 0)
-))
+        ; run all tests
+        (let ([__res test_body])
+          (display "  ")
+          (display test_name)
+          (if __res
+            [begin (display " OK") (set! __passes (+ __passes 1))]
+            [begin (display " FAIL") (set! __fails (+ __fails 1))])
+          (newline)) ...
+      ) ...
+      (newline)
+      (display "SUMMARY: ")
+      (display __passes)
+      (display " tests passed, ")
+      (display __fails)
+      (display " failed")
+      (newline))]))
 
-; Procedural replacement for the `and` syntax.
-(define andproc (lambda vs
-  (
-    (lambda (f) (f f vs))
-    (lambda (rec vs)
-      (if (null? vs)
-        #t
-        (if (car vs)
-          (rec rec (cdr vs))
-          #f
-        )
-      )
-    )
-  )
-))
+(define-syntax assert-eq (syntax-rules ()
+  [(_ actual expected)
+    (let ([__actual actual])
+      (or
+        (eq? __actual expected)
+        (begin
+          (display "Assertion failed: expected ")
+          (display 'actual)
+          (display " to be ")
+          (display 'expected)
+          (display ", but got ")
+          (display __actual)
+          (display ".")
+          (newline)
+          #f)))]))
 
 ; Factorial function.
-(define fact (lambda (x)
-  (if (eq? x 0)
-    1
-    (* x (fact (- x 1)))
-  )
-))
+(define (fact n)
+  (case n
+    [(0) 1]
+    [else (* n (fact (- n 1)))]))
 
 ; Fibonacci function.
-(define fib (lambda (n)
-  (if (eq? n 0)
-    0
-    (if (eq? n 1)
-      1
-      (+ (fib (- n 1)) (fib (- n 2)))
-    )
-  )
-))
+(define (fib n)
+  (case n
+    [(0) 0]
+    [(1) 1]
+    [else (+ (fib (- n 1)) (fib (- n 2)))]))
 
 (test-suites
-  (suite "predicates"
-    (test "eq? on same symbols" (lambda ()
-      (eq? 'a 'a)
-    ))
+  ("predicates"
+    ["eq? on same symbols" (eq? 'a 'a)]
+    ["eq? on different symbols" (not (eq? 'a 'b))]
+    ["eq? on the same number literal" (eq? 1 1)]
+    ["eq? on different number literals" (not (eq? 1 2))]
+    ["eq? on the empty list" (eq? '() '())]
+    ["atom? on a symbol" (atom? 'a)]
+    ["atom? on a list" (not (atom? '(ab c)))]
+    ["atom? on the empty list" (atom? '())])
 
-    (test "eq? on different symbols" (lambda ()
-      (not (eq? 'a 'b))
-    ))
+  ("variadics"
+    ["and" (and)]
+    ["and-t" (and #t)]
+    ["and-f" (not (and #f))]
+    ["and-t-t-t" (and #t #t #t)]
+    ["and-t-t-f" (not (and #t #t #f))]
+    ["and-t-f-t" (not (and #t #f #t))]
+    ["and-t-f-f" (not (and #t #f #f))]
+    ["and-f-t-t" (not (and #f #t #t))]
+    ["and-f-t-f" (not (and #f #t #f))]
+    ["and-f-f-t" (not (and #f #f #t))]
+    ["and-f-f-f" (not (and #f #f #f))])
 
-    (test "eq? on the same number literal" (lambda ()
-      (eq? 1 1)
-    ))
+  ("recursion"
+    ["fact-0" (assert-eq (fact 0) 1)]
+    ["fact-1" (assert-eq (fact 1) 1)]
+    ["fact-2" (assert-eq (fact 2) 2)]
+    ["fact-3" (assert-eq (fact 3) 6)]
+    ["fact-4" (assert-eq (fact 4) 24)]
+    ["fact-10" (assert-eq (fact 10) 3628800)]
+    ["fib-0" (assert-eq (fib 0) 0)]
+    ["fib-1" (assert-eq (fib 1) 1)]
+    ["fib-2" (assert-eq (fib 2) 1)]
+    ["fib-3" (assert-eq (fib 3) 2)]
+    ["fib-4" (assert-eq (fib 4) 3)]
+    ["fib-5" (assert-eq (fib 5) 5)]
+    ["fib-6" (assert-eq (fib 6) 8)]
+    ["fib-7" (assert-eq (fib 7) 13)]
+    ["fib-8" (assert-eq (fib 8) 21)]
+    ["fib-9" (assert-eq (fib 9) 34)]
+    ["fib-10" (assert-eq (fib 10) 55)])
 
-    (test "eq? on different number literals" (lambda ()
-      (not (eq? 1 2))
-    ))
-
-    (test "eq? on the empty list" (lambda ()
-      (eq? '() '())
-    ))
-
-    (test "atom? on a symbol" (lambda ()
-      (atom? 'a)
-    ))
-
-    (test "atom? on a list" (lambda ()
-      (not (atom? '(a b c)))
-    ))
-
-    (test "atom? on the empty list" (lambda ()
-      (atom? '())
-    ))
-  )
-
-  (suite "variadics"
-    (test "andproc" andproc)
-    (test "andproc-t" (lambda () (andproc #t)))
-    (test "andproc-f" (lambda () (not (andproc #f))))
-    (test "andproc-t-t-t" (lambda () (andproc #t #t #t)))
-    (test "andproc-t-t-f" (lambda () (not (andproc #t #t #f))))
-    (test "andproc-t-f-t" (lambda () (not (andproc #t #f #t))))
-    (test "andproc-t-f-f" (lambda () (not (andproc #t #f #f))))
-    (test "andproc-f-t-t" (lambda () (not (andproc #f #t #t))))
-    (test "andproc-f-t-f" (lambda () (not (andproc #f #t #f))))
-    (test "andproc-f-f-t" (lambda () (not (andproc #f #f #t))))
-    (test "andproc-f-f-f" (lambda () (not (andproc #f #f #f))))
-  )
-
-  (suite "recursion"
-    (test "fact-0" (lambda () (eq? (fact 0) 1)))
-    (test "fact-1" (lambda () (eq? (fact 1) 1)))
-    (test "fact-2" (lambda () (eq? (fact 2) 2)))
-    (test "fact-3" (lambda () (eq? (fact 3) 6)))
-    (test "fact-4" (lambda () (eq? (fact 4) 24)))
-    (test "fact-10" (lambda () (eq? (fact 10) 3628800)))
-
-    (test "fib-0" (lambda () (eq? (fib 0) 0)))
-    (test "fib-1" (lambda () (eq? (fib 1) 1)))
-    (test "fib-2" (lambda () (eq? (fib 2) 1)))
-    (test "fib-3" (lambda () (eq? (fib 3) 2)))
-    (test "fib-4" (lambda () (eq? (fib 4) 3)))
-    (test "fib-5" (lambda () (eq? (fib 5) 5)))
-    (test "fib-6" (lambda () (eq? (fib 6) 8)))
-    (test "fib-7" (lambda () (eq? (fib 7) 13)))
-    (test "fib-8" (lambda () (eq? (fib 8) 21)))
-    (test "fib-9" (lambda () (eq? (fib 9) 34)))
-    (test "fib-10" (lambda () (eq? (fib 10) 55)))
-  )
-
-  (suite "arithmetics"
-    (test "addition" (lambda () (eq? (+ 1 2) 3)))
-    (test "subtraction" (lambda () (eq? (- 3 2) 1)))
-    (test "multiplication" (lambda () (eq? (* 2 3) 6)))
-    (test "integer-division" (lambda ()
-      (andproc
-        (eq? (div 123 10) 12)
-        (eq? (div 123 -10) -12)
-        (eq? (div -123 10) -13)
-        (eq? (div -123 -10) 13)
-      )
-    ))
-    (test "integer-modulo" (lambda ()
-      (andproc
-        (eq? (mod 123 10) 3)
-        (eq? (mod 123 -10) 3)
-        (eq? (mod -123 10) 7)
-        (eq? (mod -123 -10) 7)
-      )
-    ))
-    (test "lt" (lambda ()
-      (andproc
+  ("arithmetics"
+    ["addition" (assert-eq (+ 1 2) 3)]
+    ["subtraction" (assert-eq (- 3 2) 1)]
+    ["multiplication" (assert-eq (* 2 3) 6)]
+    ["integer-division"
+      (and
+        (assert-eq (div 123 10) 12)
+        (assert-eq (div 123 -10) -12)
+        (assert-eq (div -123 10) -13)
+        (assert-eq (div -123 -10) 13)
+      )]
+    ["integer-modulo"
+      (and
+        (assert-eq (mod 123 10) 3)
+        (assert-eq (mod 123 -10) 3)
+        (assert-eq (mod -123 10) 7)
+        (assert-eq (mod -123 -10) 7)
+      )]
+    ["lt"
+      (and
         (< 1 2)
         (< 1 2 3 4 5)
         (< -1 0 1)
@@ -215,21 +138,16 @@
         (not (< 1 1))
         (not (< 1 0))
         (not (< 5 2 7 5 4))
-      )
-    ))
-    (test "misc-exprs" (lambda ()
-      (andproc
-        (eq? (div (* 5 2) (- 3)) -3)
-        (eq? (< (* 2 2) 5) #t)
-        (eq? (mod (+ 5 5) 3) 1)
-      )
-    ))
-  )
+      )]
+    ["misc-exprs"
+      (and
+        (assert-eq (div (* 5 2) (- 3)) -3)
+        (assert-eq (< (* 2 2) 5) #t)
+        (assert-eq (mod (+ 5 5) 3) 1)
+      )])
 
-  (suite "lists"
-    (test "cons" (lambda () (equal? (cons 1 2) '(1 . 2))))
-    (test "car" (lambda () (eq? (car '(1 2 3)) 1)))
-    (test "cdr" (lambda () (equal? (cdr '(1 2 3)) '(2 3))))
-    (test "cdr returns empty" (lambda () (eq? (cdr '(1)) '())))
-  )
-)
+  ("lists"
+    ["cons" (equal? (cons 1 2) '(1 . 2))]
+    ["car" (assert-eq (car '(1 2 3)) 1)]
+    ["cdr" (equal? (cdr '(1 2 3)) '(2 3))]
+    ["cdr returns empty" (assert-eq (cdr '(1)) '())]))
